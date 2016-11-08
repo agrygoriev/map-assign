64166ac7733922d19179861291163b93779822e1
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-11-08 23:32:47 +0200

    add map-assign.js

---
+++ map-assign.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const mapAssign = (map, ...sources) =>
+{
+    if (typeof map.set != "function") throw new TypeError
+
+    for (let source of sources)
+    {
+        if (source == null) continue
+
+        let entries = typeof source.entries == "function"
+            ? source.entries()
+            : Object.entries(source)
+
+        for (let entry of entries) map.set(...entry)
+    }
+
+    return map
+}
+
+module.exports = mapAssign
